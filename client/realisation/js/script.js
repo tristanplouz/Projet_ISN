51b603e4afce955f1d4c581f3db9ae492d91f648
C_orentin <29.kaserr@gmail.com>
2016-03-18 19:12:38 +0100

    Ajout du code client
    Gestion du gyroscope Javascrpt

---
+++ client/realisation/js/script.js
@@ -0,0 +1,14 @@
+var socket = io.connect("http://172.16.119.149:8080");
+
+gyro.frequency = 5;
+gyro.startTracking(function(g){
+	var f = document.getElementById("gyro");
+	var v = document.getElementById("visu")
+	f.innerHTML =
+		"<p>alpha : "+g.alpha+"</p>"+
+		"<p>beta : "+g.beta+"</p>"+
+		"<p>gamma : "+g.gamma+"</p>"
+		;
+	v.style.transform = "rotate("+g.beta+"deg)";
+	socket.emit("gyro", {alpha:g.alpha, beta:g.beta, gamma:g.gamma})
+});
