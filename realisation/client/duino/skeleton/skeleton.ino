40950b84c1e4cba42239ce4595759cfe56bf11e7
tristanplouz <tristanplouz@gmail.com>
2016-05-02 18:45:10 +0200

    ajout de la base du code arduino

---
+++ realisation/client/duino/skeleton/skeleton.ino
@@ -0,0 +1,21 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+void forward(){
+    
+}
+void backward(){
+    
+}
+void left(){
+    
+}
+void right(){
+    
+}
